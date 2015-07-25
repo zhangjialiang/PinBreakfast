830371c40faa8675763e7322525fbbb877394770
zhangjialiang <zhangjialiang@zhangjialiangdeMacBook-Pro.local>
2015-07-26 01:13:32 +0800

    day 1

--- products.config.js
+++ products.config.js
@@ -1,11 +1,118 @@
 var data = {
 	'list': [
 		{
-			'name': '煎饼果子'
+			'type': '面包',
+			'description': 'xxxx',
+			'name': '面包君1',
+			'src': 'productImgs/m1.jpg',
+			'comment': '已有2003人评价',
+			'price': 30
+
 		},
 		{
-			'name': '手抓饼'
+			'name': '面包君2',
+			'src': 'productImgs/m2.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君3',
+			'src': 'productImgs/m3.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君4',
+			'src': 'productImgs/m4.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君5',
+			'src': 'productImgs/m5.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君6',
+			'src': 'productImgs/m6.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君7',
+			'src': 'productImgs/m7.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君8',
+			'src': 'productImgs/m8.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君9',
+			'src': 'productImgs/m9.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君1',
+			'src': 'productImgs/m1.jpg',
+			'comment': '已有2003人评价'
+
+		},
+		{
+			'name': '面包君2',
+			'src': 'productImgs/m2.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君3',
+			'src': 'productImgs/m3.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君4',
+			'src': 'productImgs/m4.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君5',
+			'src': 'productImgs/m5.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君6',
+			'src': 'productImgs/m6.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君7',
+			'src': 'productImgs/m7.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君8',
+			'src': 'productImgs/m8.jpg',
+			'comment': '已有2003人评价'
+		},
+		{
+			'name': '面包君9',
+			'src': 'productImgs/m9.jpg',
+			'comment': '已有2003人评价'
 		}
+
+	],
+	'charts': [
+		{
+			'name': '牛奶1',
+			'price': 5.5,
+			'num': 2,
+			'totalPrice': 11
+		},
+		{
+			'name': '牛奶2',
+			'price': 7,
+			'num': 3,
+			'totalPrice': 21
+		}
+
+
+
 
 	]
 }
